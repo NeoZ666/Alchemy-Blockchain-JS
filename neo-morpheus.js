a72d9acccc18083d2dc444ffd76d464d757f98da
NeoZ666 <115883668+NeoZ666@users.noreply.github.com>
2023-05-22 14:56:20 +0530

    Create neo-morpheus.js

---
+++ neo-morpheus.js
@@ -0,0 +1,11 @@
+const recipient = "Neo";
+const sender = "Morpheus";
+
+// we'll interpolate these names 
+// into this email message:
+const email = `
+To ${recipient},
+
+Red pill or blue pill? 
+
+${sender}`;
