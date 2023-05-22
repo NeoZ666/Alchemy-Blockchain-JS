2c3b1a8a1b0b949e251ab54ba21875ad6697a361
NeoZ666 <115883668+NeoZ666@users.noreply.github.com>
2023-05-22 23:54:47 +0530

    IMP CONCEPT

---
+++ Notes/TruthyFalseyValues.js
@@ -0,0 +1,8 @@
+function double(x) {
+    return x * 2 || 0;
+    /* Here, the code will only return 0 for no params passed when it's written on the right side,
+       since the one on the right is the "default" value. So even if x*2 is falsey value i.e undefined
+       it will simply default to 0. */
+}
+
+module.exports = double;
