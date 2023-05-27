1c8bea9a2e287c19968968cd2d6b05e3853043a3
NeoZ666 <115883668+NeoZ666@users.noreply.github.com>
2023-05-27 15:45:38 +0530

    Explanation for the callback function

---
+++ Networks/Call back functions/index.js
@@ -0,0 +1,27 @@
+// Easy explanation of Callback function. 
+
+let message;
+
+function displayConsole(output) {
+    console.log(output);
+}
+
+function displayDOM(output) {
+    document.getElementById("myh1").innerHTML = output;
+}
+
+function setMessage(text, callback) {
+    message = text;
+    callback(message);
+}
+
+setMessage("Hey there", displayDOM)
+
+// The callback function does the same job as:
+
+function setMessage(text) {
+    message = text;
+    displayDOM(message)
+}
+
+setMessage("Hey there")
