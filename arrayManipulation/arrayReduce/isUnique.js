b4dd5de5657ce10d593d22871aeed1d041feb411
NeoZ666 <115883668+NeoZ666@users.noreply.github.com>
2023-05-27 10:14:28 +0530

    Create isUnique.js

---
+++ arrayManipulation/arrayReduce/isUnique.js
@@ -0,0 +1,17 @@
+// numbers is an array full of integers
+// let's figure if all the numbers are unique
+// i.e. [1,2,3,1] => false
+// [1,2,3] => true
+function allUnique(numbers) {
+    return numbers.reduce((accumulator, currentValue, index) => {
+        if (numbers.indexOf(currentValue)!==index) {
+            accumulator = false;
+            return accumulator;
+        }
+        else {
+            return accumulator;
+        }
+    }, true);
+}
+
+module.exports = allUnique;
